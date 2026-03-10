7973c6eb2c4b9621e9e775c652442651422b44ee
zavarich <main.zavarich@gmail.com>
2026-03-10 17:54:46 +0400

    init

---
+++ extension/data/index.js
@@ -0,0 +1,11 @@
+/**
+ * Перенаправляет пользователя на нейросетевой поиск.
+ */
+function aiSearch() {
+    window.location.href = "https://www.google.com/search?sourceid=chrome&udm=50&aep=42&source=chrome.crn.rb"
+}
+
+const aiSearchButton = document.querySelector('.ai-search-container');
+aiSearchButton.addEventListener('click', () => {
+    aiSearch();
+});
