e47d342d2955aaeb084c2e6fd93007fefcd7ea45
zavarich <main.zavarich@gmail.com>
2026-03-11 23:03:33 +0400

    табуляция 2 пробела

--- extension/data/index.js
+++ extension/data/index.js
@@ -2,10 +2,10 @@
  * Перенаправляет пользователя на нейросетевой поиск.
  */
 function aiSearch() {
-    window.location.href = "https://www.google.com/search?sourceid=chrome&udm=50&aep=42&source=chrome.crn.rb"
+	window.location.href = "https://www.google.com/search?sourceid=chrome&udm=50&aep=42&source=chrome.crn.rb"
 }
 
 const aiSearchButton = document.querySelector('.ai-search-container');
 aiSearchButton.addEventListener('click', () => {
-    aiSearch();
+	aiSearch();
 });
